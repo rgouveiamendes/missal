03177fec52aa8c3b741b5965c3c475ebf4e1ebe0
Ricardo Gouveia-Mendes <rgouveiamendes@gmail.com>
2023-12-06 23:47:55 +0000

    4 semanas Advento

--- _js/theme.js
+++
@@ -1,9 +0,0 @@
-//  Check local storage
-let localS = localStorage.getItem('theme')
-themeToSet = localS
-//  if the local storage is not set, we check the OS preference
-if (!localS === "dark"){
-themeToSet = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-}
-//  Set the correct theme
-document.documentElement.setAttribute('data-theme', themeToSet)
