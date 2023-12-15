c366c29f292b6c0aeda99e17223ce393b0b0ccd5
Murillo Teixeira <murillo.teixeira@usp.br>
2023-12-15 18:44:52 +0000

    Changing folder structure

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,13 @@
+// import adventJSON from '@/../public/data/advent.json'
+// import { DayReadings, AdventJSON } from '@/types/readings';
+// import { useState } from 'react';
+
+// export default function Teste() {
+//     const [selectedText, setSelectedText] = useState("");
+
+//     const typedAdventJSON = adventJSON as AdventJSON;
+
+//     return (
+//         <p>Teste</p>
+//     )
+// }
