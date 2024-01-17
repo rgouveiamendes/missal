1a56bb1cf16815ea8da5bf6756ded14f66da5e11
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-01-17 15:06:54 +0000

    Update Week 1 parsing to new creat_json... function
    - Also remove unnecessary some unnecessary comments.

--- _data_processing/_web_scraping/ordinary/extract_readings.py
+++ _data_processing/_web_scraping/ordinary/extract_readings.py
@@ -191,40 +191,30 @@ ordinary_readings = defaultdict(recursive_defaultdict)
 
 # Week 1 of Ordinary Time
 
-# file_paths = [
-#     "../../_old/TCSemana01.htm",
-# ]
+file_paths = [
+    "../../_old/TCSemana01.htm",
+]
 
-# weekdays = ["2", "3", "4", "5", "6", "7", 
-#             "2-even", "3-even", "4-even",
-#             "5-even", "6-even", "7-even"]
+weekdays = ["2", "3", "4", "5", "6", "7", 
+            "2-even", "3-even", "4-even",
+            "5-even", "6-even", "7-even"]
 
-# for i, file_path in enumerate(file_paths):
-#   masses_raw_text = extract_sections(file_path)
-#   # print(repr(masses_raw_text.keys()))
-#   # print(repr(masses_raw_text['I Semana do Tempo Comum']))
+for i, file_path in enumerate(file_paths):
+  masses_raw_text = extract_sections(file_path)
+  for i, key in enumerate(list(masses_raw_text.keys())[1:]):
+    # [1:] == Exclusion of first key in masses_raw_text as the 
+    #   first key refers to the initial week page containg its propers
 
-#   for i, key in enumerate(list(masses_raw_text.keys())[1:]):
-#     # [1:] == Exclusion of first key in masses_raw_text as the 
-#     #   first key refers to the initial week page containg its propers
-#     mass_by_section = get_mass_by_sections(masses_raw_text[key], possible_sections)
-    
-#     # print(f"{repr(mass_by_section.keys())}")
-#     # print(repr(mass_by_section['SALMO RESPONSORIAL - Salmo 96 (97), 1 e 2b.6 e 7c.9 (R. cf. 7c)']))
+    mass_by_section = get_mass_by_sections(masses_raw_text[key], possible_sections)
+    sections = list(mass_by_section.keys())
 
-#     sections = list(mass_by_section.keys())
+    keywords = ["EVANGELHO", "LEITURA", "ALELUIA", "SALMO"]
+    reading_idxs = [i for i, element in enumerate(sections) if any(word in element for word in keywords)]
+    # # CHATGPT: reading_idxs will contain the indices of elements in the sections list where any of the keywords are found.
 
-#     keywords = ["EVANGELHO", "LEITURA", "ALELUIA", "SALMO"]
-#     reading_idxs = [i for i, element in enumerate(sections) if any(word in element for word in keywords)]
-#     # # CHATGPT: reading_idxs will contain the indices of elements in the sections list where any of the keywords are found.
-#     # # Is a dictionary an **ordered** collection of key-value pairs, as opposed to a unorderd collection of key-value pairs?
+    readings = create_json_mass_readings(reading_idxs, mass_by_section, sections)
 
-#     readings = create_json_mass_readings(reading_idxs, mass_by_section)
-#     # print(repr(readings.keys()))
-#     # if 'gospel' in readings:
-#     #   print(f"{repr(readings['gospel'])}\n")
-
-#     ordinary_readings[f'week-{file_path[-6:-4]}'][weekdays[i]] = readings
+    ordinary_readings[f'week-{file_path[-6:-4]}'][weekdays[i]] = readings
 
 
 # print(repr(ordinary_readings.keys()))
