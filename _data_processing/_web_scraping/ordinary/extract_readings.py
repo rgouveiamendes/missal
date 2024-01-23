f5611c4cf356493dfebebd1bf4a587a3ea651ecd
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-01-23 10:58:05 +0000

    Go over Ordinary Time Week 31 readings extraction
    Seemingly bug free. Only some details:
    - Quarta-feira, Quinta-feira - Ano II:
      psalm's reference ends with a space.

--- _data_processing/_web_scraping/ordinary/extract_readings.py
+++ _data_processing/_web_scraping/ordinary/extract_readings.py
@@ -290,6 +290,7 @@ file_paths = [
   # "../../_old/TCSemana28.htm",
   # "../../_old/TCSemana29.htm",
   # "../../_old/TCSemana30.htm",
+  "../../_old/TCSemana31.htm",
 ]
 
 weekdays = ["1", "1", "1", 
@@ -330,38 +331,38 @@ print(repr(ordinary_readings.keys()))
 # print(repr(ordinary_readings['week-06']['2'].keys()))
 # print(repr(ordinary_readings['week-06']['2-even'].keys()))
 
-# for sunday in ordinary_readings['week-06']['1']:
-#   print(f"{sunday.keys()}\n")
-#   print(f"{sunday['reading-I']}\n")
-#   if 'alt-reading-I--1' in sunday:
-#     print(f"{sunday['alt-reading-I--1']}\n")
-#   print(f"{sunday['psalm']}\n")
-#   print(f"{sunday['reading-II']}\n")
-#   if 'alt-reading-II--1' in sunday:
-#     print(f"{sunday['alt-reading-II--1']}\n")
-#   print(f"{sunday['aleluia']}\n")
-#   print(f"{sunday['gospel']}\n")
-#   if 'alt-gospel--1' in sunday:
-#     print(f"{sunday['alt-gospel--1']}\n")
+for sunday in ordinary_readings['week-31']['1']:
+  print(f"{sunday.keys()}\n")
+  print(f"{sunday['reading-I']}\n")
+  if 'alt-reading-I--1' in sunday:
+    print(f"{sunday['alt-reading-I--1']}\n")
+  print(f"{sunday['psalm']}\n")
+  print(f"{sunday['reading-II']}\n")
+  if 'alt-reading-II--1' in sunday:
+    print(f"{sunday['alt-reading-II--1']}\n")
+  print(f"{sunday['aleluia']}\n")
+  print(f"{sunday['gospel']}\n")
+  if 'alt-gospel--1' in sunday:
+    print(f"{sunday['alt-gospel--1']}\n")
 
-# for day in ordinary_readings['week-13']:
-#   if 'reading-I' in ordinary_readings['week-13'][day] and 'gospel' not in ordinary_readings['week-13'][day]:
-#     print(f"{ordinary_readings['week-13'][day]['reading-I']}\n")
-#     if 'alt-reading-I--1' in  ordinary_readings['week-13'][day]:
-#       print(f"{ordinary_readings['week-13'][day]['alt-reading-I--1']}\n")
-#     print(f"{ordinary_readings['week-13'][day]['psalm']}\n")
+for day in ordinary_readings['week-31']:
+  if 'reading-I' in ordinary_readings['week-31'][day] and 'gospel' not in ordinary_readings['week-31'][day]:
+    print(f"{ordinary_readings['week-31'][day]['reading-I']}\n")
+    if 'alt-reading-I--1' in  ordinary_readings['week-31'][day]:
+      print(f"{ordinary_readings['week-31'][day]['alt-reading-I--1']}\n")
+    print(f"{ordinary_readings['week-31'][day]['psalm']}\n")
 
     
-# for day in ordinary_readings['week-06']:
-#   if 'gospel' in ordinary_readings['week-06'][day]:
-#     print(f"{ordinary_readings['week-06'][day]['reading-I']}\n")
-#     if 'alt-reading-I--1' in  ordinary_readings['week-06'][day]:
-#       print(f"{ordinary_readings['week-06'][day]['alt-reading-I--1']}\n")
-#     print(f"{ordinary_readings['week-06'][day]['psalm']}\n")
-#     if 'aleluia' in ordinary_readings['week-06'][day]:
-#       print(f"{ordinary_readings['week-06'][day]['aleluia']}\n")
-#     print(f"{ordinary_readings['week-06'][day]['gospel']}\n")
-#     if 'alt-gospel--1' in ordinary_readings['week-06'][day]:
-#       print(f"{ordinary_readings['week-06'][day]['alt-gospel--1']}\n")
+for day in ordinary_readings['week-31']:
+  if 'gospel' in ordinary_readings['week-31'][day]:
+    print(f"{ordinary_readings['week-31'][day]['reading-I']}\n")
+    if 'alt-reading-I--1' in  ordinary_readings['week-31'][day]:
+      print(f"{ordinary_readings['week-31'][day]['alt-reading-I--1']}\n")
+    print(f"{ordinary_readings['week-31'][day]['psalm']}\n")
+    if 'aleluia' in ordinary_readings['week-31'][day]:
+      print(f"{ordinary_readings['week-31'][day]['aleluia']}\n")
+    print(f"{ordinary_readings['week-31'][day]['gospel']}\n")
+    if 'alt-gospel--1' in ordinary_readings['week-31'][day]:
+      print(f"{ordinary_readings['week-31'][day]['alt-gospel--1']}\n")
 
 
